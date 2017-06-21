54ea89e80ae9e9892dc496d3f36d50d0982857b9
A-mIn3 <jerwhite0@gmail.com>
2017-06-21 17:13:32 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -79,11 +79,11 @@ int  ssl_connect(sslsocket* s){
                 if(SSL_connect(s->sslhandle)!=1)
                         return 0;
 
+                return 1;
+        
         }else
         
-                return 0;
-
-        return 1;
+                return 0;  
 
 }
 
